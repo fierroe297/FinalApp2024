5398c1a5f79c3d14904d9c8e3ea709f14222c3f9
fierroe297 <fier9958@bears.unco.edu>
2024-05-06 19:44:40 -0600

    Initial commit for vercel deployment

---
+++ finalapp/src/App.js
@@ -0,0 +1,16 @@
+// App.js
+
+import React from 'react';
+import Header from './components/Header';
+
+const App = () => {
+    return (
+      
+        <div className="app">
+            <Header />
+            {/* Other components go here */}
+        </div>
+    );
+};
+
+export default App;
---
+++ finalapp/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <header>
+        <h1>Final App</h1>
+        </header>
+    );
+    }
+// Path: finalapp/src/components/Header.css
